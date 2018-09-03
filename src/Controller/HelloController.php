91a8851740da8c168bf659af86702ecdfb20e457
Behram ÇELEN <behramcelen@gmail.com>
2018-09-03 03:14:50 +0200

    servis oluşturma - unutulan route

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,9 +4,15 @@ namespace App\Controller;
 
 use App\Service\NameGenerator;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    /**
+     * @Route("/hello")
+     * @param NameGenerator $nameGenerator
+     * @return Response
+     */
     public function hello(NameGenerator $nameGenerator)
     {
         $name = $nameGenerator->randomName();
