c5635e599b8fbd065415aad687888a98398ad2a7
Behram ÇELEN <behramcelen@gmail.com>
2018-09-03 06:06:34 +0200

    Merhaba Dünya

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function index()
+    {
+        return new JsonResponse(['message' => 'Merhaba Dünya!']);
+    }
+}
