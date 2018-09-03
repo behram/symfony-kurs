cc1b7014f942cab9b77ea0966ef65977ee039b26
Behram ÇELEN <behramcelen@gmail.com>
2018-09-04 00:41:18 +0200

    dosya isimlendirilmesi

---
+++ src/Controller/TemplateController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TemplateController extends AbstractController
+{
+    /**
+     * @Route("/template", name="index")
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render("template/index.html.twig");
+    }
+}
