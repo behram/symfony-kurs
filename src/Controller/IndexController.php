764746442e2c8daa4e9b1e46813703054e560ac0
Behram ÇELEN <behramcelen@gmail.com>
2018-09-03 06:35:28 +0200

    request ve response yapısı

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +17,47 @@ class IndexController extends AbstractController
     public function index()
     {
         return new JsonResponse(['message' => 'Merhaba Dünya!']);
+    }
+
+    /**
+     * @Route("/request", name="request_test")
+     * @param RequestStack $requestStack
+     * @return Response
+     */
+    public function requestTest(RequestStack $requestStack)
+    {
+        $request = $requestStack->getCurrentRequest();
+
+        // $_POST
+        $name = $request->request->get('name');
+
+        // $_GET
+        $name = $request->query->get('name', 'Orhan');
+
+        // $_COOKIE
+        $request->cookies->get('username');
+
+        // karşılığı yok
+        $request->attributes->get('name');
+
+        // $_FILES
+        $request->files->get('filename');
+
+        // $_SERVER
+        $serverData = $request->server->get('REMOTE_ADDR');
+
+        $headers = $request->headers->all();
+
+        return new Response('Request');
+    }
+
+    /**
+     * @Route("/response", name="response_test")
+     * @param RequestStack $requestStack
+     * @return Response
+     */
+    public function responseTest(RequestStack $requestStack)
+    {
+        return $this->redirectToRoute('request_test');
     }
 }
