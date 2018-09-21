2c54c4cd2b2c5f262e82fd8391d98426e79729ce
Behram ÇELEN <behramcelen@gmail.com>
2018-09-21 17:03:30 +0200

    önbellekleme (caching) kullanımı

--- src/Controller/CacheController.php
+++ src/Controller/CacheController.php
@@ -3,32 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
-use Symfony\Component\Cache\Adapter\RedisAdapter;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class CacheController extends Controller
 {
     /**
      * @Route("/cache-test")
-     * @param Request $request
-     * @param AdapterInterface $cache
-     * @return Response
-     * @throws \Psr\Cache\InvalidArgumentException
      */
-    public function new(Request $request, AdapterInterface $cache)
+    public function new(AdapterInterface $cache)
     {
-        $client = RedisAdapter::createConnection('redis://127.0.0.1');
-        $redis = new RedisAdapter($client);
-        $jale = $redis->getItem('jale');
-        $jale->set('Canımsın');
-        $redis->save($jale);
-
-        dump($jale);
-
-
         $cache->hasItem('isim');
 
         $item = $cache->getItem('isim');
@@ -39,8 +24,6 @@ class CacheController extends Controller
 
         $cache->getItem('isim');
 
-        $cache->deleteItem('isim');
-
-        return new Response('Hello');
+        return new Response('<html><body>Hello</body></html>');
     }
 }
